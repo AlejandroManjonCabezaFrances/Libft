dd4c7c60dde8225339273a8cbee25dba6645c7b5
AlejandroManjonCabezaFrances <manjon.alejandro@gmail.com>
2024-02-20 15:10:04 +0100

    ft_strjoin with NULL ok

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: amanjon- <amanjon-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/26 07:21:28 by amanjon-          #+#    #+#             */
-/*   Updated: 2024/02/20 15:07:25 by amanjon-         ###   ########.fr       */
+/*   Updated: 2024/02/20 15:09:41 by amanjon-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		i;
 	int		j;
 
-	//if (s1 == NULL || s2 == NULL)
-	//	return (NULL);
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	ptr = malloc(sizeof(char) * (ft_strlen((char *)s1)
 				+ft_strlen((char *)s2)) + 1);
 	if (ptr == NULL)
@@ -35,11 +35,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (ptr);
 }
 
-int main(void)
+/*int main(void)
 {
 	char	s1[] = "hola ";
 	char	s2[] = "caracola";
 
 	printf("%s", ft_strjoin(s1, s2));
     return(0);
-}
+}*/
